60cb4cab712c06d812e060a34ec38b8c20a65723
KristinaTomash <kristitomsh@gmail.com>
2025-09-17 19:06:23 +0300

    add

---
+++ src/main/java/com/phonebook/data/ContactData.java
@@ -0,0 +1,10 @@
+package com.phonebook.data;
+
+public class ContactData {
+    public static final String NAME = "KRIS";
+    public static final String LASTNAME = "TOMASH";
+    public static final String PHONE = "KRIS";
+    public static final String EMAIL = "Test@gm.com";
+    public static final String ADDRESS = "REHOVOT";
+    public static final String DESCRIPTION = "QA";
+}
---
+++ src/main/java/com/phonebook/data/UserData.java
@@ -0,0 +1,6 @@
+package com.phonebook.data;
+
+public class UserData {
+    public static final  String EMAIL = "kris99@gmail.com";
+    public static final String PASSWORD = "Aa12345!";
+}
